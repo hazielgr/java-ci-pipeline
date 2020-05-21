25b40f27f0991db26670da24857e4167f737f007
hazielgr <mariohaziel@gmail.com>
2020-05-21 16:34:53 -0600

    TravisTest

--- src/basicCalculator/Calculate.java
+++ src/basicCalculator/Calculate.java
@@ -17,6 +17,6 @@ public class Calculate {
 	
 	public static void main(String[] args) {
 		Calculate calculadora = new Calculate();
-		System.out.println(calculadora.sumar(8,8));
+		System.out.println(calculadora.sumar(9,9));
 	}
 }
